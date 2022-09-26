02b062a017c9a98e06f92d1b06963fa5f29e5476
Igor Pereira <igorpereira120@gmail.com>
2022-09-25 22:22:19 -0300

    Project created

---
+++ src/main/java/br/com/fiap/challengesprint/ChallengesprintApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengesprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengesprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengesprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/challengesprint/ChallengesprintApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengesprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengesprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
